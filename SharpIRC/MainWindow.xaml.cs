66712f082b18249d5b71c5af2b81ca9ee6e5759a
Tama Waddell <me@tama.id.au>
2013-11-13 17:12:55 +1000

    Better UI
    Introduced a new better UI with a metro like feel

--- SharpIRC/MainWindow.xaml.cs
+++ SharpIRC/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,13 +17,24 @@ using MahApps.Metro.Controls;
 namespace SharpIRC
 {
     /// <summary>
-    /// Interaction logic for Window1.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter) return;
+
+            var tBox = (TextBox) sender;
+            Debug.WriteLine(tBox.Text);
+
+            // if it ends in a slash process command in a command 
+
         }
     }
 }
---
+++ SharpIRC/UserCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpIRC
+{
+    public class UserCommand
+    {
+        public const string CommandStart = "\\";
+
+        public enum SharpIRCCommand
+        {
+            Help,
+            Connect,
+            Disconnect,
+            Join,
+            Leave,
+            Exit,
+            Clear,
+            Say
+        }
+
+        public SharpIRCCommand Command { get; set; }
+        public List<string> Params { get; set; }
+
+        private UserCommand()
+        {
+            Params = new List<string>();
+        }
+
+        static private UserCommand Decode(string commandString)
+        {
+            var userCommand = new UserCommand();
+
+            if (!commandString.StartsWith(CommandStart))
+                return userCommand; // empty command
+
+            string[] commandAndParameters = commandString.Split(' ');
+
+            //string[] commandAndParameters = message.Substring(prefixEnd + 1, trailingStart - prefixEnd - 1).Split(' ');
+
+            return userCommand;
+        }
+
+    }
+}
